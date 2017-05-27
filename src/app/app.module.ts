fe27af85e6926ca99428956952cec6ec22daeb9b
Wedson Quintanilha <wquintanilhadasilva@gmail.com>
2017-05-27 12:01:19 -0300

    configurando o Locale para os pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { ExemplosPipesComponent } from './exemplos-pipes/exemplos-pipes.component';
 import { CamelCasePipe } from './pipes/camel-case.pipe';
+import { SettingsService } from './settings.service';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,30 @@ import { CamelCasePipe } from './pipes/camel-case.pipe';
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [
+    /*
+     * {
+        provide:LOCALE_ID,
+        useValue: 'pt-BR'
+      },
+     */
+    /*
+      //Sintaxe possível para esta seção
+      {
+        provide:LOCALE_ID,
+        useValue: 'pt-BR' //use este OU
+        useFactory:       //use este OU
+        useClass:         //use este
+      }
+    */
+    //Agora de outra forma, obtendo o locale de um serviço de configuração e não hardcoded
+    SettingsService,
+    {
+      provide:LOCALE_ID,
+      deps:[SettingsService],
+      useFactory: (service:SettingsService) => service.getLocale()
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
